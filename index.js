622b0d05796227ff03e34394d4101f2767e381c4
Andrea Parodi <andrea@parro.it>
2017-11-18 17:38:31 +0100

    first version

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-export default function isAsyncIterable() {
-  return 42;
+/**
+ * Return true if the argument is async iterable
+ * @param  {any}  val value to check
+ * @return {Boolean}     true if the value is async iterable
+ */
+export default function isAsyncIterable(val) {
+  const isIterable = typeof val[Symbol.iterator] === "function";
+  const isAsync = typeof val[Symbol.asyncIterator] === "function";
+  // ? console.log({ val, isIterable, isAsync, s: Symbol.asyncIterator });
+  return isAsync || isIterable;
 }
--- test.js
+++ test.js
@@ -1,6 +1,22 @@
 import test from "tape-async";
 import isAsyncIterable from ".";
 
-test("exports a function", t => {
+test("exports a function", async t => {
   t.is(typeof isAsyncIterable, "function");
+});
+
+test("return false for non async iterables", async t => {
+  t.false(isAsyncIterable(0));
+});
+
+test("return true for async iterables", async t => {
+  t.true(isAsyncIterable((async function*() {})()));
+});
+
+test("return true for iterables - array", async t => {
+  t.true(isAsyncIterable([]));
+});
+
+test("return true for iterables - string", async t => {
+  t.true(isAsyncIterable(""));
 });
