1e27c6cdec5c844cdb8a7f65d3b88900897bcf2b
Andrea Parodi <andrea@parro.it>
2017-11-18 22:17:38 +0100

    removed log;

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ export default function isAsyncIterable(val) {
   }
   const isIterable = typeof val[Symbol.iterator] === "function";
   const isAsync = typeof val[Symbol.asyncIterator] === "function";
-  // ? console.log({ val, isIterable, isAsync, s: Symbol.asyncIterator });
+
   return isAsync || isIterable;
 }
